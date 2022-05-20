5292bdb994e1e4fbf04bc5bd51cf635c88e411fa
NimaFaghihi <nimafaghihi99@gmail.com>
2022-05-20 09:57:59 +0200

    HTML-kod
    Lagt lite html kod vi kan utgå ifrån.

--- weatherly/src/App.js
+++ weatherly/src/App.js
@@ -1,10 +1,32 @@
 import SmhiAPI from "./SmhiAPI";
 
+
 function App() {
   return (
     <div className="App">
-      <h1>Weatherly</h1>
-      <SmhiAPI />
+      <div className="container">
+        <div className="row">
+          <div className="col-md-2">
+          </div>
+          <div className="col">
+            <div className="text-center">
+              <h1>Weatherly</h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+                molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+                numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+                optio, eaque rerum!
+              </p>
+            </div>
+            <br></br>
+            <hr></hr>
+            <SmhiAPI />
+            <weatherList />
+          </div>
+          <div className="col-md-2">
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- weatherly/src/SmhiAPI.js
+++ weatherly/src/SmhiAPI.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import test from './static/day.svg'
 
 export default function SmhiAPI() {
 
@@ -21,11 +22,34 @@ export default function SmhiAPI() {
     }
 
     return (
-        <div><h1>SmhiAPI.js komponent</h1>
+        <div>
+            <h2>7-Dygnsprognos</h2>
             <div>
+                <table className="table">
+                    <thead>
+                        <tr>
+                        <th scope="col">Dag</th>
+                        <th scope="col">Dygn</th>
+                        <th scope="col">L</th>
+                        <th scope="col">H</th>
+                        <th scope="col">Vind</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr className="clickable-row" data-href="#">
+                        <td><img src={test} alt=""></img></td>
+                        <td>20 Maj</td>
+                        <td>14°</td>
+                        <td>19°</td>
+                        <td>4 m/s</td>
+                        </tr>
+                    </tbody>
+
+                </table>    
                 <button className="btn btn-lg btn-success" onClick={getWeather}>Get started</button>
             </div>
 
         </div>
+        
     )
 }
