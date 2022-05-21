f420ced80f80df99e825c21cc4cab66d24279dd9
TobiasSvenssonDev <tobias_svensson84@live.se>
2022-05-21 16:58:36 +0200

    Rättat fel och omarbetat get request
    Flyttat en del HTML till App.js. Överproblematiserade axios till en början men nu har jag nog fått bättre kläm på det.
    Problemet var att jag aldrig hann få svaret från SMHI i tid. provade med async och await men fick inte riktigt till det ändå.
    
    if (!weather) return null; var nog en del av lösningen där. Tillsammans med ett vettigare formulerat get-request förstås.

--- weatherly/src/App.js
+++ weatherly/src/App.js
@@ -1,8 +1,10 @@
-import SmhiAPI from "./SmhiAPI";
+import React from "react";
 import WeatherList from "./WeatherList";
-
+import SmhiAPI from "./SmhiAPI";
 
 function App() {
+  const weekday = new Date().toLocaleDateString(undefined, { weekday: 'long' });
+
   return (
     <div className="App">
       <div className="container">
@@ -21,7 +23,27 @@ function App() {
             </div>
             <br></br>
             <hr></hr>
-            <SmhiAPI />
+            <div>
+              <h2>7-Dygnsprognos</h2>
+              <div>
+                <table className="table">
+                  <thead>
+                    <tr>
+                      <th scope="col">Dag</th>
+                      <th scope="col">Väder</th>
+                      <th scope="col">Temp</th>
+                      <th scope="col">Vind</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <td>
+                      {weekday}
+                    </td>
+                    <SmhiAPI />
+                  </tbody>
+                </table>
+              </div>
+            </div>
             <WeatherList />
           </div>
           <div className="col-md-2">
--- weatherly/src/SmhiAPI.js
+++ weatherly/src/SmhiAPI.js
@@ -1,65 +1,32 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import axios from 'axios';
 import test from './static/day.svg'
 
+const baseURL = "https://opendata-download-metanalys.smhi.se/api/category/mesan1g/version/2/geotype/point/lon/16.158/lat/58.5812/data.json";
+
 export default function SmhiAPI() {
-    const url = 'https://opendata-download-metanalys.smhi.se/api/category/mesan1g/version/2/geotype/point/lon/16.158/lat/58.5812/data.json';
-    const [weather, setWeather] = useState({
-        day: 'test',
-            forecast: 2,
-            temp: 1 + "°C",
-            highTemp: "temp°",
-            windSpeed: 3
+    const [weather, setWeather] = React.useState(null);
 
+    React.useEffect(() => {
+        axios.get(baseURL).then((response) => {
+            console.log(response.data);
+            setWeather(response.data);
         });
-    
-    axios.get(url).then(function (response) {
+    }, []);
 
-        console.log("SUCCESS");
-        console.log(response.data)
-        const weekday = new Date();
-        const options = { weekday: 'long' };
+    if (!weather) return null;
 
-        setWeather([...weather] = {
-            day: weekday.toLocaleDateString(undefined, options),
-            forecast: response.data.timeSeries[0].parameters[22].values[0],
-            temp: response.data.timeSeries[0].parameters[0].values[0] + "°C",
-            highTemp: "temp°",
-            windSpeed: response.data.timeSeries[0].parameters[6].values[0]
-        });
-
-
-    })
-        .catch(function (error) {
-            console.log(error + " FEL");
-        });
-
-
-    return (
-        <div>
-            <h2>7-Dygnsprognos</h2>
-            <div>
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">Dag</th>
-                            <th scope="col">Dygn</th>
-                            <th scope="col">Temp</th>
-                            <th scope="col">Vind</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr className="clickable-row" data-href="#">
-                            <td><img src={test} alt=""></img></td>
-                            <td>{weather.day}</td>
-                            <td>{weather.temp}</td>
-                            <td>{weather.windSpeed}m/s</td>
-                        </tr>
-                    </tbody>
-
-                </table>
-            </div>
-        </div>
-
+    return (<>
+        <td>
+            <img src={test} alt="" />
+            {weather.timeSeries[0].parameters[22].values[0]}
+        </td>
+        <td>
+            {weather.timeSeries[0].parameters[0].values[0]}
+        </td>
+        <td>
+            {weather.timeSeries[0].parameters[6].values[0]}m/s
+        </td>
+    </>
     )
 }
