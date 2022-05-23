4b6c321958e04c51d91657d639a0ae4189d602cb
TobiasSvenssonDev <tobias_svensson84@live.se>
2022-05-23 14:58:08 +0200

    Update SmhiAPI.js
    Vi hade råkat ta fel adress för väderprognoser... vi hade väderhistorik

--- weatherly/src/SmhiAPI.js
+++ weatherly/src/SmhiAPI.js
@@ -2,8 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import test from './static/day.svg'
 
-const baseURL = "https://opendata-download-metanalys.smhi.se/api/category/mesan1g/version/2/geotype/point/lon/16.158/lat/58.5812/data.json";
-
+const baseURL = "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/16.158/lat/58.5812/data.json";
 export default function SmhiAPI(props) {
     const [weather, setWeather] = React.useState(null);
 
