7b110469ccb88432b8156694e42937a8705e512d
Stenberg <81235010+Axel-Heinonen@users.noreply.github.com>
2022-05-31 16:42:18 +0200

    added Long and Lat as consts

--- weatherly/src/Geolocation.js
+++ weatherly/src/Geolocation.js
@@ -4,9 +4,12 @@ if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(getPosition);
   }
   function getPosition(position) {
-    console.log("LOCATION" + position.coords.latitude, position.coords.longitude);
+    const lat = position.coords.latitude;
+    const long =  position.coords.longitude;
+    console.log("long = " + long)
+    console.log("Lat = " + lat)
   }
-  
+
 
 export default function Geolocation() {
   return (
--- weatherly/src/SmhiAPI.js
+++ weatherly/src/SmhiAPI.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Weather from './Weather';
+import Geolocation from './Geolocation';
 
 export default function SmhiAPI(props) {
     const [weather, setWeather] = useState([]);
@@ -13,10 +14,13 @@ export default function SmhiAPI(props) {
         const dateTimeToString = currentDate.getFullYear() + "-" + ('0' + (currentDate.getMonth() + 1)).slice(-2) + "-" + currentDate.getDate() + "T" + currentDate.getHours() + ":00:00Z";
         const weekday = currentDate.toLocaleDateString(undefined, { weekday: 'long' });
         const capitalWeekday = weekday.slice(0, 1).toUpperCase() + weekday.slice(1, weekday.length)
-        let urlLongitude = 16.158
-        let urlLatitude = 58.5812            
+        
+        //Const under borde hämtas från Geolocation.js, 
+        //men lyckas inte få det att funka, men att bryta ut dem i komponten funkar fint
+        const lat = position.coords.latitude;
+        const long =  position.coords.longitude;          
 
-        const baseURL = "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/"+urlLongitude+"/lat/"+urlLatitude+"/data.json"
+        const baseURL = "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/"+long+"/lat/"+lat+"/data.json"
         axios.get(baseURL).then((response) => {
             const SMHIData = [response.data]
             //STÄDA BORT UTSKRIFT
