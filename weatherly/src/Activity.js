b523bd96b91c55ff946d57a7de01764eb9ef03af
TobiasSvenssonDev <tobias_svensson84@live.se>
2022-06-03 13:37:18 +0200

    Update Activity.js
    Skrev först en massa jobbig kod men det blev ganska invecklat efter ett tag... kommenterade bort den för att kuna återanvända sådant som ändå fungerar men skriver om allt för att ta tillvara på vad react faktiskt tillför!

--- weatherly/src/Activity.js
+++ weatherly/src/Activity.js
@@ -1,24 +1,36 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+/*
+function getStorage(){
+  let storage = JSON.parse(localStorage.getItem("suggested"));
+  if (storage == null){
+    const emptyStorage = []
+    localStorage.setItem("suggested", JSON.stringify(emptyStorage));
+    storage = JSON.parse(localStorage.getItem("suggested"));
+    return storage;
+  } else {
+    return storage;
+  }
+}
 
 function saveLocalStorage(activity){
-  //const dateTimeToString = currentDate.getFullYear() + "-" + ('0' + (currentDate.getMonth() + 1)).slice(-2) + "-" + currentDate.getDate() + "T" + currentDate.getHours() + ":00:00Z";
   const now = new Date();
   const timeStamp = now.getFullYear()+':'+('0' + (now.getMonth() + 1)).slice(-2)+':'+now.getDate()+':'+now.getHours()+':'+now.getMinutes()+':00'
   console.log(now + " Date")
   console.log(timeStamp + " Expiration")
+  const usedActivities = getStorage()
+  console.log(usedActivities + " KOLLAHÄRDÅFÖRFAAAAN")
+  console.log(typeof(usedActivities))
   const activityObject = {
     activity: activity,
     timeStamp: timeStamp
   }
-  localStorage.setItem("suggested", JSON.stringify(activityObject));
+  usedActivities.push(activityObject);
+  console.log(usedActivities + " SKITAPP")
+  
+  localStorage.setItem("suggested", JSON.stringify(usedActivities));
 }
 
-export default function Activity(props) {
-    console.log(props.activityCode + "KOD")
-    
-
-    const activities = {
+const activities = {
       "goodWeather":[{"text":"Spela fotboll", "search": "fotboll"}, {"text": "Bada", "search": "Badplats"}, 
       {"text": "Kasta frisbee", "search": "frisbee"}, {"text": "Spela minigolf", "search": "minigolf"}, 
       {"text": "Vandra", "search": "Vandringsled"}, {"text": "Grilla", "search": "Grillplats"}],
@@ -41,10 +53,50 @@ export default function Activity(props) {
         saveLocalStorage(activity);
       };
 
+*/
+export default function Activity(props) {
+    const [usedActivity, setUsedActivity] = useState(null);
+    const [activities, setActivity] = useState(JSON.parse(localStorage.getItem("activities")));
+    const [randomActivity, setRandomActivity] = useState(null);
+    console.log(activities["goodWeather"][0]["text"] + " RANDOM")
+
+    const activity = true;
+    useEffect(()=>{
+      const activities = {
+        "goodWeather":[{"text":"Spela fotboll", "search": "fotboll"}, {"text": "Bada", "search": "Badplats"}, 
+        {"text": "Kasta frisbee", "search": "frisbee"}, {"text": "Spela minigolf", "search": "minigolf"}, 
+        {"text": "Vandra", "search": "Vandringsled"}, {"text": "Grilla", "search": "Grillplats"}],
+        "badWeather":[{"text": "Spela laserdome", "search": "laserdome"}, {"text": "Kör go-cart", "search": "Gocart"},
+        {"text": "Dra och bowla", "search": "bowling"}, {"text": "Spela biljard", "search": "Biljard"}, 
+        {"text": "Dra till gymmet", "search": "Gym"}, {"text": "Åk till gallerian", "search": "köpcenter"}, {"text": "spela tv-spel", "search":"Arkad"}, {"text": "spela laserdome", "search": "Laserdome"}]
+      }
+      localStorage.setItem("activities", JSON.stringify(activities));
+    }, []);
+
+    useEffect(()=>{
+      let activity = null;
+      while (activity === null){
+      if (props.activityCode <= 5) {
+        activity = activities.goodWeather[Math.floor(Math.random() * activities.goodWeather.length)];
+        console.log(activity["text"] + "UTOMHUSAKTIVITET");
+        
+      } else if (props.activityCode >= 6) {
+        activity = activities.badWeather[Math.floor(Math.random() * activities.goodWeather.length)];
+        console.log(activity["text"] + "INOMHUSAKTIVITET");
+      }}
+      setRandomActivity(activity["text"])
+
+    },[activities]);
+
+  
+
+
+    
+
     if (activity) {
   return (<div>
     <h1><u>Activity</u></h1>
-    <h2>{activity.text}</h2>
+    <h2>{randomActivity}</h2>
     </div>
   )
 }
