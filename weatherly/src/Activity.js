ff426955a65b7bbff0ac73c9958e79463dc1d21e
Stenberg <81235010+Axel-Heinonen@users.noreply.github.com>
2022-06-03 11:50:57 +0200

    added activities in new format

--- weatherly/src/Activity.js
+++ weatherly/src/Activity.js
@@ -5,11 +5,13 @@ export default function Activity(props) {
     console.log(props.activityCode + "KOD")
 
     const activities = {
-      "goodWeather":[{"text":"Spela fotboll", "search": "fotboll"}, {"text": "Bada", "search": "Bada"}, 
-      {"text": "Kasta frisbee", "search": "frisbee"}, {"text": "Spela minigolf", "search": "minigolf"}],
+      "goodWeather":[{"text":"Spela fotboll", "search": "fotboll"}, {"text": "Bada", "search": "Badplats"}, 
+      {"text": "Kasta frisbee", "search": "frisbee"}, {"text": "Spela minigolf", "search": "minigolf"}, 
+      {"text": "Vandra", "search": "Vandringsled"}, {"text": "Grilla", "search": "Grillplats"}],
 
-      "badWeather":[{"text": "Spela laserdome", "search": "laserdome"}, {"text": "Kör go-cart", "search": "gocart"},
-      {"text": "Dra och bowla", "search": "bowling"}, {"text": "Spela biljard", "search": "biljard"}],
+      "badWeather":[{"text": "Spela laserdome", "search": "laserdome"}, {"text": "Kör go-cart", "search": "Gocart"},
+      {"text": "Dra och bowla", "search": "bowling"}, {"text": "Spela biljard", "search": "Biljard"}, 
+      {"text": "Dra till gymmet", "search": "Gym"}, {"text": "Åk till gallerian", "search": "köpcenter"}, {"text": "spela tv-spel", "search":"Arkad"}, {"text": "spela laserdome", "search": "Laserdome"}],
     }
 
     let activity = null;
