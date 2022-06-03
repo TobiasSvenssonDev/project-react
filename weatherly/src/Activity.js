0b5b89cb065061246144909e7372bd74cb48006d
TobiasSvenssonDev <tobias_svensson84@live.se>
2022-06-03 12:02:14 +0200

    Update Activity.js

--- weatherly/src/Activity.js
+++ weatherly/src/Activity.js
@@ -1,29 +1,43 @@
 import React from 'react'
-import { useState, useEffect } from "react";
+
+
+function saveLocalStorage(activity){
+  //const dateTimeToString = currentDate.getFullYear() + "-" + ('0' + (currentDate.getMonth() + 1)).slice(-2) + "-" + currentDate.getDate() + "T" + currentDate.getHours() + ":00:00Z";
+  const now = new Date();
+  const timeStamp = now.getFullYear()+':'+('0' + (now.getMonth() + 1)).slice(-2)+':'+now.getDate()+':'+now.getHours()+':'+now.getMinutes()+':00'
+  console.log(now + " Date")
+  console.log(timeStamp + " Expiration")
+  const activityObject = {
+    activity: activity,
+    timeStamp: timeStamp
+  }
+  localStorage.setItem("suggested", JSON.stringify(activityObject));
+}
 
 export default function Activity(props) {
     console.log(props.activityCode + "KOD")
+    
 
     const activities = {
-      "goodWeather":[{"text":"Spela fotboll", "search": "fotboll"}, {"text": "Bada", "search": "Bada"}, 
-      {"text": "Kasta frisbee", "search": "frisbee"}, {"text": "Spela minigolf", "search": "minigolf"}],
+      "goodWeather":[{"id": 1, "text":"Spela fotboll", "search": "fotboll"}, {"id": 2, "text": "Bada", "search": "Bada"}, 
+      {"id": 3, "text": "Kasta frisbee", "search": "frisbee"}, {"id": 4, "text": "Spela minigolf", "search": "minigolf"}],
 
-      "badWeather":[{"text": "Spela laserdome", "search": "laserdome"}, {"text": "Kör go-cart", "search": "gocart"},
-      {"text": "Dra och bowla", "search": "bowling"}, {"text": "Spela biljard", "search": "biljard"}],
+      "badWeather":[{"id": 1, "text": "Spela laserdome", "search": "laserdome"}, {"id": 2, "text": "Kör go-cart", "search": "gocart"},
+      {"id": 3, "text": "Dra och bowla", "search": "bowling"}, {"id": 4, "text": "Spela biljard", "search": "biljard"}]
     }
 
     let activity = null;
     if (props.activityCode <= 5) {
         activity = activities.goodWeather[Math.floor(Math.random() * activities.goodWeather.length)];
-        console.log(activity + "UTOMHUSAKTIVITET");
+        console.log(activity["text"] + "UTOMHUSAKTIVITET");
 
-        localStorage.setItem("suggested", JSON.stringify(activity));
+        saveLocalStorage(activity);
 
       } else if (props.activityCode >= 6) {
         activity = activities.badWeather[Math.floor(Math.random() * activities.goodWeather.length)];
         console.log(activity + "INOMHUSAKTIVITET");
 
-        localStorage.setItem("suggested", JSON.stringify(activity));
+        saveLocalStorage(activity);
       };
 
     if (activity) {
